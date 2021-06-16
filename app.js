e67e3321c6940fd9d0419033e7ba8b5455fee23f
WagnerReis <wagnerreis1997@gmail.com>
2021-06-16 14:38:14 -0300

    Processador de dados

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const Reader = require("./Reader");
+const Processor = require("./Processor");
 
 var leitor = new Reader();
 
 async function main() {
   var dados = await leitor.Read("./users.csv");
-  console.log(dados);
+  var dadosProcessados = Processor.process(dados);
+  
 }
 
 main();
